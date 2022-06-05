1bdaea9751ced444d107fb2f01675986c9510821
johanhartono <johanhartono@gmail.com>
2022-06-05 22:54:39 +0700

    First Commit

---
+++ android/app/src/main/kotlin/com/example/bwadesigntocodeday1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bwadesigntocodeday1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bwadesigntocodeday1/first_splash.dart';
+import 'package:flutter/material.dart';
+import 'first_splash.dart';
+
+void main() {
+  runApp(const SevenDays());
+}
+
+class SevenDays extends StatelessWidget {
+  const SevenDays({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Seven Days Flutter Demo',
+        home: FirstSplash()
+        );
+  }
+}
